b3c9cea77e3fd6101eff6c9e0cfb9b809610a2c9
Israel Poscai <iposcai8@gmail.com>
2020-08-17 14:43:52 -0300

    teste01

---
+++ Documents/NetBeansProjects/testegit/src/testegit/Testegit.java
@@ -0,0 +1,15 @@
+
+package testegit;
+
+public class Testegit {
+
+    public static void main(String[] args) {
+        int x = 140;
+        
+        System.out.println("Teste Git");
+        System.out.println(x);
+        
+        
+    }
+    
+}
